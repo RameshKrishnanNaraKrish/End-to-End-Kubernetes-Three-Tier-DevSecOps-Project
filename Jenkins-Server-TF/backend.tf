b99bb9ef627efd48f0201eefc328bcc4dd9d28e0
RameshKrishnanNaraKrish <nkrk1996@outlook.com>
2024-09-27 17:18:32 -0400

    my changes

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "mys3-3tire-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
@@ -9,7 +9,7 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = ">= 3.6.1"
       source  = "hashicorp/aws"
     }
   }
